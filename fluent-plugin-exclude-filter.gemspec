27ba3d927458f531b67a209bf5e5b8b82adc6644
root <root@localhost.localdomain>
2014-08-10 15:21:41 +0900

    init tree

---
+++ fluent-plugin-exclude-filter.gemspec
@@ -0,0 +1,22 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+Gem::Specification.new do |spec|
+  spec.name          = "fluent-plugin-exclude-filter"
+  spec.version       = "0.0.1" 
+  spec.authors       = ["yu yamada"]
+  spec.email         = ["yu.yamada07@gmail.com"]
+  spec.description   = %q{Output filter plugin to exclude records}
+  spec.summary       = %q{Output filter plugin to exclude records}
+  spec.homepage      = "://github.com/yu-yamada/fluent-plugin-exclude-filter"
+  spec.license       = "MIT"
+
+  spec.files         = `git ls-files`.split($/)
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "bundler", "~> 1.3"
+  spec.add_development_dependency "rake"
+end
---
+++ lib/fluent/plugin/out_exclude_filter.rb
@@ -0,0 +1,75 @@
+require 'yaml'
+class Fluent::ExcludeFilterOutput < Fluent::Output
+  Fluent::Plugin.register_output('exclude-filter', self)
+
+  config_param :key, :string, :default => nil
+  config_param :value, :string, :default => nil
+  config_param :file_path, :string, :default => nil
+  config_param :regexp, :bool, :default => false
+  config_param :add_tag_prefix, :string, :default => 'exclude'
+
+  def initialize
+    super
+  end
+
+  def configure(conf)
+    super
+
+    @key = @key.to_s
+    @value = @value.to_s 
+    @tag_prefix = "#{@add_tag_prefix}."
+
+    @tag_proc =
+      if @tag_prefix
+        Proc.new {|tag| "#{@tag_prefix}#{tag}" }
+      else
+        Proc.new {|tag| tag }
+      end
+
+   @excludes_yml = nil
+   if @file_path
+     @excludes_yml = YAML.load_file(@file_path)
+   end 
+  end
+
+  def emit(tag, es, chain)
+    emit_tag = @tag_proc.call(tag)
+
+    es.each do |time,record|
+      if @key && @value
+        next if match(@key, @value, record) 
+      end
+      if @excludes_yml
+        next if yml_match(record)
+      end 
+
+      Fluent::Engine.emit(emit_tag, time, record)
+    end
+
+    chain.next
+  end
+
+  def yml_match(record)
+    @excludes_yml.each{|k,v| 
+      if v.kind_of?(Array)
+        v.each{|va|
+          if match(k, va, record)
+            return true
+          end
+        }
+      else
+        if match(k, v, record)
+          return true
+        end
+      end
+    }
+    return false
+  end
+  def match(k, v, record)
+    if @regexp
+      return Regexp.compile(v.to_s).match(record[k])
+    else
+      return record[k] == v.to_s
+    end
+  end 
+end
